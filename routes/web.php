17ddf6c343d95a71338e2eb85b869a33ba15b09b
Junio <junio@MacBook-Pro-de-Junio.local>
2024-01-15 09:55:25 -0300

    Puxando usuario que criou e atualizou os registros

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/reset', function () {
+    Artisan::call('storage:link');
+    return view('welcome');
+});
